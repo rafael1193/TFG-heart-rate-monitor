691df67177424f8f707f4c077fa1d18be47b6b2e
rafael1193 <rafaelbailon@ieee.org>
2015-06-11 13:20:28 +0200

    Initial code for android app

---
+++ src/Pulsometro/app/src/main/java/rafael1193/pulsometro/WellcomeActivity.java
@@ -0,0 +1,45 @@
+package rafael1193.pulsometro;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class WellcomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wellcome);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_wellcome, menu);
+        return true;
+    }
+
+    public void onClickStartButton (View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
