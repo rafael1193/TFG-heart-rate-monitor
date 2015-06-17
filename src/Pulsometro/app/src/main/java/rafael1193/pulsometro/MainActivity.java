8b8ac1b69b2de938253829d9a4f3b19915d932ef
Rafael Bailón <rafaelbailon@ieee.org>
2015-06-17 15:08:45 +0200

    Define plot component

--- src/Pulsometro/app/src/main/java/rafael1193/pulsometro/MainActivity.java
+++ src/Pulsometro/app/src/main/java/rafael1193/pulsometro/MainActivity.java
@@ -6,14 +6,50 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+
+import com.androidplot.xy.PointLabelFormatter;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private XYPlot plot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plot = (XYPlot) findViewById(R.id.XYPlot);
+
+        plot.getLegendWidget().setVisible(false);
+
+        // Create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {70, 65, 83, 72, 75, 68};
+
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                String.valueOf(R.string.plot_bpm_series));                             // Set the display title of the series
+
+        // Create a formatter to use for drawing a series using LineAndPointRenderer
+        // and configure it from xml:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter();
+        series1Format.setPointLabelFormatter(new PointLabelFormatter());
+        series1Format.configure(getApplicationContext(),
+                R.xml.line_point_formatter_with_plf1);
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+
+        // reduce the number of range labels
+        plot.setTicksPerRangeLabel(6);
+        //plot.getGraphWidget().setDomainLabelOrientation(-45);
     }
 
     @Override
@@ -24,8 +60,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onClickHelloButton (View v) {
-        TextView hello_text = (TextView) findViewById(R.id.hello_textView);
-        hello_text.setText(R.string.hello_universe);
+        //TextView hello_text = (TextView) findViewById(R.id.hello_textView);
+        //hello_text.setText(R.string.hello_universe);
     }
 
     @Override
