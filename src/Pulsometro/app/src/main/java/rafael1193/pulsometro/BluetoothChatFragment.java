2c32d664bc550440d94e28a9d809dde427c94949
Rafael Bailón <rafaelbailon@ieee.org>
2015-06-28 20:16:49 +0200

    Remove menu items

--- src/Pulsometro/app/src/main/java/rafael1193/pulsometro/BluetoothChatFragment.java
+++ src/Pulsometro/app/src/main/java/rafael1193/pulsometro/BluetoothChatFragment.java
@@ -437,17 +437,6 @@ public class BluetoothChatFragment extends Fragment {
                 startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
                 return true;
             }
-            case R.id.insecure_connect_scan: {
-                // Launch the DeviceListActivity to see devices and do scan
-                Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
-                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
-                return true;
-            }
-            case R.id.discoverable: {
-                // Ensure this device is discoverable by others
-                ensureDiscoverable();
-                return true;
-            }
         }
         return false;
     }
